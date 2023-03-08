6840ed59356ae912dbd243c7bd93589ff62ccb2f
Ishit <ishitsingh2001@gmail.com>
2023-03-09 00:14:10 +0530

    chore : initialize basic setup for frontend

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+
+import './App.css';
+import React from 'react';
+import { Header,Footer } from './components';
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <Footer />
+      <h1>React App</h1>
+    </div>
+  );
+}
+
+export default App;
